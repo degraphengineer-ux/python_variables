7a345afeb2262ed1bda80b6987280972fd33b757
Degraph Engine <degraphengineer@gmail.com>
2026-09-07 16:47:13 +0300

    Initial commit

---
+++ intro.py
@@ -0,0 +1,9 @@
+name = "Michael"
+age = 18
+favourite_language = "Python"
+goal = "Become a software engineer"
+
+print("My name is", name + ".")
+print("I am", age, "years old.")
+print("My favourite programming language is", favourite_language + ".")
+print("My goal for learning this course is to", goal + ".")
